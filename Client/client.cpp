6892f29c2c89b9d15bc54e47251f45f2fb05f72f
hhhxiao <1417599491@qq.com>
2020-06-24 01:18:24 +0800

    rename some variables

--- Client/client.cpp
+++ Client/client.cpp
@@ -8,18 +8,18 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <cstdio>
-#include "../utils.h"
+#include "../Utils.h"
 #include <cstring>
 #include <thread>
 #include <vector>
 #include <mutex>
 
-#define BUFF_SIZE 1024
+#define BUFF_SIZE 256
 
 std::mutex mutex;
 
 //upload function
-void buildConnect(sockaddr_in *address, int id, long *timeList) {
+void buildConnect(sockaddr_in *address, int id, long *timeList, size_t *bytes) {
     int sock_fd = socket(PF_INET, SOCK_STREAM, 0);
     expect(sock_fd != -1, "socket create failure\n");
     int result = connect(sock_fd, (struct sockaddr *) address, sizeof(*address));
@@ -31,8 +31,10 @@ void buildConnect(sockaddr_in *address, int id, long *timeList) {
     int len;
     while (true) {
         len = fread((void *) buffer, 1, BUFF_SIZE, fp);
+        printf("thread %d: send [%d] bytes\n", id, len);
         if (len < BUFF_SIZE) {
             write(sock_fd, buffer, len);
+            bytes[id] += len;
             break;
         }
         write(sock_fd, buffer, BUFF_SIZE);
@@ -58,17 +60,28 @@ int main(int argc, const char *argv[]) {
     server_address.sin_addr.s_addr = inet_addr(argv[1]);
     server_address.sin_port = htons(atoi(argv[2]));
     int thread_num = atoi(argv[3]);
+
     std::vector<std::thread> threadList;
     long *timeList = new long[thread_num];
+    auto *bytes = new size_t[thread_num];
+
     threadList.reserve(thread_num);
     for (int i = 0; i < thread_num; ++i)
-        threadList.emplace_back(buildConnect, &server_address, i, timeList);
+        threadList.emplace_back(buildConnect, &server_address, i, timeList, bytes);
     for (auto &thread :threadList)
         thread.join();
-    long total = 0;
-    for (int i = 0; i < thread_num; i++)
-        total += timeList[i];
-    printf("\n %d average time is %ld us\n", thread_num, total / thread_num);
+    long totalTime = 0;
+    size_t totalBytes = 0;
+    for (int i = 0; i < thread_num; i++) {
+        totalTime += timeList[i];
+        totalBytes += bytes[i];
+    }
+    printf("\nuse %d threads, average send %lu bytes time is %ld us\n",
+           thread_num,
+           totalBytes / thread_num,
+           totalTime / thread_num
+    );
+
     delete[] timeList;
     return 0;
 }
